9afb5da1b2cdbf1a219c31f749ceb66c65317b05
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2026-02-07 21:59:11 +0500

    auth issues

--- Xlya/auth/src/user-sync/app.py
+++ Xlya/auth/src/user-sync/app.py
@@ -1,11 +1,15 @@
 import boto3
 import os
 from datetime import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['USERS_TABLE'])
 
-def handler(event, context):
+def lambda_handler(event, context):
     user_attrs = event['request']['userAttributes']
     item = {
         'cognito_sub': user_attrs['sub'],
@@ -14,7 +18,9 @@ def handler(event, context):
         'last_name': user_attrs.get('custom:last_name', ''),
         'user_status': 'ACTIVE',
         'onboarding_status': True,
+        'coins': 50,
         'created_at': datetime.utcnow().isoformat()
     }
     table.put_item(Item=item)
+    logger.info(f"User inserted into DynamoDB: {user_attrs['sub']}")
     return event
